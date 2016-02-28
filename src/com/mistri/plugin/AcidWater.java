ef02d37a7518292a641152fe173f82dd8c15599a
Mistri <jmistri7@gmail.com>
2016-02-28 11:20:11 -0800

    Added world support

--- src/com/mistri/plugin/AcidWater.java
+++ src/com/mistri/plugin/AcidWater.java
@@ -15,9 +15,9 @@ public class AcidWater extends JavaPlugin implements Listener
 {
 	Manager manager;
 	FileManager fileManager;
-	
+
 	List<String> defaultWorlds = Arrays.asList("world", "world_nether", "world_the_end");
-	
+
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		String prefix = (ChatColor.GREEN + "[" + ChatColor.AQUA + "AcidWater" + ChatColor.GREEN + "] " + ChatColor.GRAY);
 		String error = (ChatColor.RED + "");
@@ -26,14 +26,52 @@ public class AcidWater extends JavaPlugin implements Listener
 		if (sender instanceof Player) {
 			Player p = (Player) sender;
 			if (commandLabel.equalsIgnoreCase("acidwater") && args.length == 0) {
-				p.sendMessage(prefix + "AcidWater v1.1 made by " + ChatColor.GOLD + "Mistri");
+				p.sendMessage(prefix + "AcidWater v1.2 made by " + ChatColor.GOLD + "Mistri");
 				p.sendMessage(ChatColor.RED + "Usage:");
 				p.sendMessage(ChatColor.AQUA + "/AcidWater " + ChatColor.GRAY + "- " + ChatColor.GOLD + "Bring up this help menu.");
 				p.sendMessage(ChatColor.AQUA + "/AcidWater setDamage <amount> " + ChatColor.GRAY + "- " + ChatColor.GOLD + "Sets the amount of hearts a player is damaged by while in water. Measured by half hearts.");
 				p.sendMessage(ChatColor.AQUA + "/AcidWater setTicks <amount> " + ChatColor.GRAY + "- " + ChatColor.GOLD + "Sets the amount of ticks in between each time a player is damaged while in water.");
-				p.sendMessage(ChatColor.AQUA + "/AcidWater worldSupport <on/off> " + ChatColor.GRAY + "- " + ChatColor.GOLD + "Turns world support on or off.");
+				p.sendMessage(ChatColor.AQUA + "/AcidWater worldSupport [on/off] " + ChatColor.GRAY + "- " + ChatColor.GOLD + "Turns world support on or off. Without the last argument, doing this command will notify you if world support is currently on or off.");
+				p.sendMessage(ChatColor.AQUA + "/AcidWater worlds " + ChatColor.GRAY + "- " + ChatColor.GOLD + "Will show you which worlds are currently on the world support list.");
 				p.sendMessage(ChatColor.AQUA + "/AcidWater addWorld <name> " + ChatColor.GRAY + "- " + ChatColor.GOLD + "If world support is on, this will add a world to damage players in.");
 				p.sendMessage(ChatColor.AQUA + "/AcidWater removeWorld <name> " + ChatColor.GRAY + "- " + ChatColor.GOLD + "If world support is on, this will remove a world to damage players in.");
+			}
+			else if (commandLabel.equalsIgnoreCase("acidwater") && args.length == 1) {
+				if (p.hasPermission("AcidWater.worldSupport")) {
+					if (args[0].equalsIgnoreCase("worldSupport")) {
+						if (fileManager.getConfig("config.yml").get().getBoolean("world-support")) {
+							p.sendMessage(prefix + "World support is on. To disable it, do " + ChatColor.AQUA + "/AcidWater worldSupport off" + ChatColor.GRAY + ".");
+						}
+						else {
+							p.sendMessage(prefix + "World support is off. To enable it, do " + ChatColor.AQUA + "/AcidWater worldSupport on" + ChatColor.GRAY + ".");
+						}
+					}
+				}
+				else {
+					p.sendMessage(prefix + noPermission);
+				}
+				if (p.hasPermission("AcidWater.worlds")) {
+					if (args[0].equalsIgnoreCase("worlds")) {
+						List<String> worlds = fileManager.getConfig("config.yml").get().getStringList("worlds");
+						if (fileManager.getConfig("config.yml").get().getBoolean("world-support")) {
+							p.sendMessage(prefix + "Current worlds " + ChatColor.RED + "(WORLD SUPPORT IS ON)" + ChatColor.GRAY + ":");
+							for (String s : worlds) {
+								p.sendMessage(ChatColor.AQUA + "- " + s);
+							}
+							p.sendMessage(prefix + "To add a world, do " + ChatColor.AQUA + "/AcidWater addWorld <world>");
+						}
+						else {
+							p.sendMessage(prefix + "Current worlds " + ChatColor.RED + "(WORLD SUPPORT IS OFF)" + ChatColor.GRAY + ":");
+							for (String s : worlds) {
+								p.sendMessage(ChatColor.AQUA + "- " + s);
+							}
+							p.sendMessage(prefix + "To add a world, do " + ChatColor.AQUA + "/AcidWater addWorld <world>");
+						}
+					}
+				}
+				else {
+					p.sendMessage(prefix + noPermission);
+				}
 			}
 			else if (commandLabel.equalsIgnoreCase("acidwater") && args.length == 2) {
 				if (args[0].equalsIgnoreCase("setdamage")) {
@@ -73,12 +111,12 @@ public class AcidWater extends JavaPlugin implements Listener
 						if (args[1].equalsIgnoreCase("on")) {
 							fileManager.getConfig("config.yml").get().set("world-support", true);
 							fileManager.saveConfig("config.yml");
-							p.sendMessage(prefix + "Sucessfully turned world support on. Please note that in order for this change to take affect, you need to reload the server by doing" + error + " /reload" + ChatColor.GRAY + ".");
+							p.sendMessage(prefix + "Sucessfully turned world support on.");
 						}
 						else if (args[1].equalsIgnoreCase("off")) {
 							fileManager.getConfig("config.yml").get().set("world-support", false);
 							fileManager.saveConfig("config.yml");
-							p.sendMessage(prefix + "Sucessfully turned world support off. Please note that in order for this change to take affect, you need to reload the server by doing" + error + " /reload" + ChatColor.GRAY + ".");
+							p.sendMessage(prefix + "Sucessfully turned world support off.");
 						}
 						else {
 							p.sendMessage(prefix + error + "Incorrect usage of command. Do " + ChatColor.BLUE + "/AcidWater " + error + "to view help.");
@@ -93,7 +131,8 @@ public class AcidWater extends JavaPlugin implements Listener
 						List<String> addThisWorld = fileManager.getConfig("config.yml").get().getStringList("worlds");
 						addThisWorld.add(args[1].toString());
 						fileManager.getConfig("config.yml").get().set("worlds", addThisWorld);
-						p.sendMessage(prefix + "Successfully added world " + error + args[1] + ChatColor.GRAY + " to the worlds list. To remove it, do " + ChatColor.BLUE + "/AcidWater removeWorld " + args[1] + ChatColor.GRAY + ".");
+						fileManager.getConfig("config.yml").save();
+						p.sendMessage(prefix + "Successfully added world " + ChatColor.AQUA + args[1] + ChatColor.GRAY + " to the worlds list. To remove it, do " + ChatColor.AQUA + "/AcidWater removeWorld " + args[1] + ChatColor.GRAY + ".");
 					}
 					else {
 						p.sendMessage(prefix + noPermission);
@@ -104,7 +143,8 @@ public class AcidWater extends JavaPlugin implements Listener
 						List<String> removeThisWorld = fileManager.getConfig("config.yml").get().getStringList("worlds");
 						removeThisWorld.remove(args[1].toString());
 						fileManager.getConfig("config.yml").get().set("worlds", removeThisWorld);
-						p.sendMessage(prefix + "Successfully removed world " + error + args[1] + ChatColor.GRAY + " from the worlds list. To add it back, do " + ChatColor.BLUE + "/AcidWater addWorld " + args[1] + ChatColor.GRAY + ".");
+						fileManager.getConfig("config.yml").save();
+						p.sendMessage(prefix + "Successfully removed world " + ChatColor.AQUA + args[1] + ChatColor.GRAY + " from the worlds list. To add it back, do " + ChatColor.AQUA + "/AcidWater addWorld " + args[1] + ChatColor.GRAY + ".");
 					}
 					else {
 						p.sendMessage(prefix + noPermission);
